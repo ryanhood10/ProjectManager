9559b5a1ace69a9ce534c6697c6ac54b9ba497ef
Ryan Hood <114943155+ryanhood10@users.noreply.github.com>
2023-01-25 19:53:47 -0600

    add starter files

---
+++ index.js
@@ -0,0 +1,5 @@
+
+//pull in routes
+
+const events = require('./routes/events');
+app.use('/events', events);
---
+++ routes/events..js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res) {
+  // code to retrieve events
+});
+
+router.post('/', function(req, res) {
+  // code to create new events
+});
+
+module.exports = router;
