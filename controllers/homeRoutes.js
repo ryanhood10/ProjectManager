6a67d554fd2b6dd38a247b0f277dec1a3380cc62
CoulsonP <coulsonp@icloud.com>
2023-01-30 11:30:18 -0600

    added models and controllers for user

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    res.render('homepage', { 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', async (req, res) => {
+    try {
+      res.render('login', { 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
+
+
+module.exports = router
--- index.js
+++
@@ -1,5 +0,0 @@
-
-//pull in routes
-
-const events = require('./routes/events');
-app.use('/events', events);
